1ce1589f46e84bf3649b8aaf9bb08c2f1269dafd
Kaylee <kayleeoberzan@gmail.com>
2020-03-15 18:05:38 -0500

    Parts 1-4 3.15.20

--- queries.sql
+++ queries.sql
@@ -1,7 +1,16 @@
 ## Part 1: Test it with SQL
+SSELECT * FROM job;
 
 ## Part 2: Test it with SQL
+SELECT name FROM employer WHERE location="St. Louis";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT job.name, skill.name, skill.description
+FROM job
+JOIN job_skills ON job.id = job_skills.jobs_id
+JOIN skill ON job_skills.skills_id = skill.id
+WHERE job.name IS NOT NULL
+ORDER BY job.name ASC;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,37 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    @Size (min = 10, max = 500, message = "Skill must be between 10 and 500 characters.")
+    @NotBlank
+    private String description;
+
+    public Skill(){}
+
+    public Skill (String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
